71c081a4f1d72f82c7002e0a52263ba24f75bba5
Nikolai Trushnikov <63305246+niktru95@users.noreply.github.com>
2024-11-28 17:07:19 +0300

    Некоторые изменения

--- pages/inventory_page.ts
+++ pages/inventory_page.ts
@@ -2,15 +2,20 @@ import { type Locator, type Page } from "@playwright/test";
 
 export class inventoryPage {
     readonly page: Page;
-    readonly product_sort: Locator;
-    readonly product_name: Locator;
+    // readonly product_sort: Locator;
+    // readonly product_name: Locator;
+    readonly link_back_to_products: Locator;
 
     constructor(page: Page) {
-        this.product_name = page.getByText('Sauce Labs Backpack');
+        // this.product_name = page.getByText('Sauce Labs Backpack');
+        this.link_back_to_products = page.locator('[data-test="item-4-title-link"]');
     }
 
-    async click_link_product() {
-        await this.product_name.click();
-    }
+    // async click_link_product() {
+    //     await this.product_name.click();
+    // }
     
+    async click_link_back_to_products() {
+        await this.link_back_to_products.click();
+    }
 }
--- tests/inventory-page.spec.ts
+++ tests/inventory-page.spec.ts
@@ -11,8 +11,37 @@ test.beforeEach('Авторизация', async ({ page }) => {
     await expect(page.getByText('Products')).toBeVisible();
 })
 
-test('Проверка наличия данных в карточке товара', async ({ page }) => {
-    await allure.displayName("Проверка наличия данных в карточке");
+test('Проверка наличия данных в карточке товара на главной странице', async ({ page }) => {
+    await allure.displayName("Проверка наличия данных в карточке на главной странице");
+
+    await allure.step('В карточке товара отображается описание', async => {
+        expect(page
+        .getByText('carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.'))
+        .toBeVisible();
+    })
+
+    await allure.step('В карточке товара отображается цена', async => {
+        expect(page.getByText('$29.99')).toBeVisible();
+    })
+
+    await allure.step('В карточке товара отображается кнопка "Добавить в корзину"', async => {
+        expect(page.getByRole('button', { name: 'Add to cart' })
+        .first())
+        .toBeVisible();
+    })
+})
+
+test('Переход на страницу товара', async ({ page }) => {
+    await allure.displayName("Переход на страницу товара");
+
+    const inventory_page = new inventoryPage(page);
+    await allure.step('Клик на тайтл продукта', async => {
+        inventory_page.click_link_back_to_products();
+    })
+
+    await allure.step('Урл должен вести на страницу товара', async => {
+        expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=4');
+    })
 
     await allure.step('В карточке товара отображается описание', async => {
         expect(page
