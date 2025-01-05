f93fe6023ccbadbd060dbda82fe54a095e22baf1
Nikolai Trushnikov <63305246+niktru95@users.noreply.github.com>
2025-01-05 23:59:47 +0300

    Добавил тесты для бокового меню

--- pages/sidebar_page.ts
+++ pages/sidebar_page.ts
@@ -1,12 +1,26 @@
-// import { Locator, type Page } from "@playwright/test";
-//
-// export class sidebarPage {
-//     readonly page: Page;
-//
-//     constructor(page: Page) {
-//         this.all_items_item = page.getByText('All Items');
-//         this.about_item = page.getByText('About');
-//         this.logout_item = page.getByText('Logout');
-//         this.reset_app_state_item = page.getByText('Reset App State');
-//     }
-// }
+import { Locator, type Page } from "@playwright/test";
+
+export class sidebarPage {
+    readonly page: Page;
+    readonly burger_menu: Locator;
+    readonly all_items_item: Locator;
+    readonly about_item: Locator;
+
+    constructor(page: Page) {
+        this.burger_menu = page.getByRole('button', { name: 'Open Menu' });
+        this.all_items_item = page.getByText('All Items');
+        this.about_item = page.getByText('About');
+    };
+
+    async clickBurgerMenu () {
+        await this.burger_menu.click();
+    };
+
+    async clickAllItems () {
+        await this.all_items_item.click();
+    };
+
+    async clickAboutItem () {
+        await this.about_item.click();
+    }
+}
--- tests/sidebar-page.spec.ts
+++ tests/sidebar-page.spec.ts
@@ -1,5 +1,8 @@
+// Тест на кнопку Logout написан в файле logout_page
+// Тест на кнопку Reset App State написан не будет (состояние сайта не меняется)
+
 import { test, expect } from '@playwright/test';
-// import { sidebarPage } from '../pages/sidebar_page';
+import { sidebarPage } from '../pages/sidebar_page';
 import * as allure from "allure-js-commons";
 
 test.beforeEach('Аутентификация', async ({ page }) => {
@@ -14,19 +17,67 @@ test.beforeEach('Аутентификация', async ({ page }) => {
 test('Проверка наличия элементов в боковом меню', async ({page}) => {
     await allure.displayName('Проверка наличия элементов в в боковом меню');
 
+    const sidebar_page = new sidebarPage(page);
+
+    await allure.step('Открыть боковое меню', async () => {
+        await sidebar_page.clickBurgerMenu();
+    });
+
     await allure.step('В боковом меню содержится кнопка All items', async () => {
         await expect(page.getByText('All Items')).toBeVisible();
-    })
+    });
 
     await allure.step('В боковом меню содержится кнопка About', async () => {
         await expect(page.getByText('About')).toBeVisible();
-    })
+    });
 
     await allure.step('В боковом меню содержится кнопка Logout', async () => {
         await expect(page.getByText('Logout')).toBeVisible();
-    })
+    });
 
     await allure.step('В боковом меню содержится кнопка Reset App State', async () => {
         await expect(page.getByText('Reset App State')).toBeVisible();
-    })
-})
+    });
+});
+
+
+test('Клик на кнопку All items ведет на главную страницу', async ({page}) => {
+    await allure.displayName('Клик на кнопку All items ведет на главную страницу');
+
+    const sidebar_page = new sidebarPage(page);
+
+    await allure.step('Перейти на страницу товара', async () => {
+        await page.goto('https://www.saucedemo.com/inventory-item.html?id=4');
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=4');
+    });
+
+    await allure.step('Открыть боковое меню', async () => {
+        await sidebar_page.clickBurgerMenu();
+    });
+
+    await allure.step('Кликнуть на кнопку All items', async () => {
+        await sidebar_page.clickAllItems();
+    });
+
+    await allure.step('Открылась главная страница с товарами', async () => {
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    });
+});
+
+test('Клик на кнопку About ведет страницу о проекте Sauce Lab', async ({page}) => {
+    await allure.displayName('Клик на кнопку About ведет на главную страницу');
+
+    const sidebar_page = new sidebarPage(page);
+
+    await allure.step('Открыть боковое меню', async () => {
+        await sidebar_page.clickBurgerMenu();
+    });
+
+    await allure.step('Кликнуть на кнопку About', async () => {
+        await sidebar_page.clickAboutItem();
+    });
+
+    await allure.step('Открылась страница о проекте Sauce Lab', async () => {
+        await expect(page).toHaveURL('https://saucelabs.com/');
+    });
+});
